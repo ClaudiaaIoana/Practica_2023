53aa92021a2eb92bca80f448f1e89a1670ec3daf
ClaudiaaIoana <102853455+ClaudiaaIoana@users.noreply.github.com>
2023-06-27 22:26:08 +0300

    segmentation of offline side

--- main.sh
+++ main.sh
@@ -3,9 +3,12 @@
 traffic_stat()
 {
 	echo "----------------------------"
-	num_packets=`sudo tshark -r $1 2> /dev/null | wc -l` 
-	time=`sudo tshark -r $1 2> /dev/null | tail -n 1 | tr -s " " | cut -f3 -d" "`
-	total_size=$(sudo tshark -r $1 -T fields -e frame.len 2> /dev/null | awk '{s+=$1} END {print s}')
+	
+	let lvl=$1-1
+	
+	num_packets=`sudo tshark -r filtering$lvl 2> /dev/null | wc -l` 
+	time=`sudo tshark -r filtering$lvl 2> /dev/null | tail -n 1 | tr -s " " | cut -f3 -d" "`
+	total_size=$(sudo tshark -r filtering$1 -T fields -e frame.len 2> /dev/null | awk '{s+=$1} END {print s}')
 	echo "$total_size"
 	
 	vol_packets=$(echo "scale=2; $num_packets / $time" | bc -l || echo "error: time=0")
@@ -14,14 +17,14 @@ traffic_stat()
 	vol_bytes=$(echo "scale=2; $total_size / $time" | bc -l || echo "error: time=0")
 	echo "Bytes/sec = $vol_bytes"
 	
-	src_ips=`sudo tshark -r $1 -T fields -e ip.src -e ipv6.src 2> /dev/null | sort | uniq | wc -l`
-	dest_ips=`sudo tshark -r $1 -T fields -e ip.dst -e ipv6.dst  2> /dev/null | sort | uniq | wc -l`
+	src_ips=`sudo tshark -r filtering$lvl -T fields -e ip.src -e ipv6.src 2> /dev/null | sort | uniq | wc -l`
+	dest_ips=`sudo tshark -r filtering$lvl -T fields -e ip.dst -e ipv6.dst  2> /dev/null | sort | uniq | wc -l`
 	
 	echo "Nr. source ip = $src_ips"
 	echo "Nr. destination ip = $dest_ips"
 	
-	src_ports=`sudo tshark -r $1 -T fields -e ip.src -e ipv6.src -e tcp.srcport -e udp.srcport 2> /dev/null | sort | uniq | wc -l`
-	dest_ports=`sudo tshark -r $1 -T fields -e ip.dst -e ipv6.dst -e tcp.srcport -e udp.srcport 2> /dev/null | sort | uniq | wc -l`
+	src_ports=`sudo tshark -r filtering$lvl -T fields -e ip.src -e ipv6.src -e tcp.srcport -e udp.srcport 2> /dev/null | sort | uniq | wc -l`
+	dest_ports=`sudo tshark -r filtering$lvl -T fields -e ip.dst -e ipv6.dst -e tcp.srcport -e udp.srcport 2> /dev/null | sort | uniq | wc -l`
 	
 	echo "Nr. source ports = $src_ports"
 	echo "Nr. destination ports = $dest_ports"
@@ -31,28 +34,55 @@ traffic_stat()
 ip_source()
 {
 	echo "----------------------------"
+	
+	let lvl=$1-1
+	touch filtering$1
+	
 	read -p "Introduce ip: " ip
-	sudo tshark -r $1 -Y "ip.src == $ip" -w $1 2> /dev/null || sudo tshark -r $1 -Y "ipv6.src == $ip" -w $1 2> /dev/null
-	sudo tshark -r $1 2> /dev/null | egrep "$ip → "
+	sudo tshark -r filtering$lvl -Y "ip.src == $ip" -w filtering$1 2> /dev/null || sudo tshark -r filtering$lvl -Y "ipv6.src == $ip" -w filtering$1 2> /dev/null
+	sudo tshark -r filtering$1 2> /dev/null | egrep "$ip → "
 	echo "----------------------------"
 }
 
 ip_destination()
 {
 	echo "----------------------------"
+	
+	let lvl=$1-1
+	touch filtering$1
+	
 	read -p "Introduce ip: " ip
-	sudo tshark -r $1 -Y "ip.dst == $ip" -w $1 2> /dev/null || sudo tshark -r $1 -Y "ipv6.dst == $ip" -w $1 2> /dev/null
-	sudo tshark -r $1 2> /dev/null | egrep "→ $ip"
+	sudo tshark -r filtering$lvl -Y "ip.dst == $ip" -w filtering$1 2> /dev/null || sudo tshark -r filtering$lvl -Y "ipv6.dst == $ip" -w filtering$1 2> /dev/null
+	sudo tshark -r filtering$1 2> /dev/null | egrep "→ $ip"
 	echo "----------------------------"
 }
 
 protocol()
 {
 	echo "----------------------------"
+	
+	let lvl=$1-1
+	touch filtering$1
+	
 	read -p "Introduce protocol: " prot
-	sudo tshark -r $1 -Y "$prot" -w $1 2> /dev/null
-	sudo tshark -r $1
+	sudo tshark -r filtering$lvl -Y "$prot" -w filtering$1 2> /dev/null
+	sudo tshark -r filtering$1
 	echo "----------------------------"
+}
+
+interaction()
+{
+	let lvl=$1-1
+	touch filtering$1
+	
+	read -p "Introduce ip source: " ip
+	sudo tshark -r filtering$lvl -Y "ip.dst == $ip" -w filtering$1 2> /dev/null || sudo tshark -r filtering$lvl -Y "ipv6.dst == $ip" -w filtering$1 2> /dev/null
+	
+	read -p "Introduce ip destination: " ip
+	sudo tshark -r filtering$1 -Y "ip.dst == $ip" -w filtering$1 2> /dev/null || sudo tshark -r filtering$1 -Y "ipv6.dst == $ip" -w filtering$1 2> /dev/null
+	
+	tshark -r filtering$1
+	
 }
 
 word_alert()
@@ -69,19 +99,6 @@ evidence()
 	echo "$1 $2" >> levels
 
 }
-
-online_ip_source()
-{
-	#sudo pkill -SIGTERM tshark
-	read -p "Introduce ip: " ip
-	touch filtering$1
-	let pv=$1-1
-	(sudo tshark -Y "ip.src == $ip" -r <(tail -f filtering$pv) -w filtering$1
- 2>/dev/null & disown) || (sudo tshark -Y "ipv6.src == $ip"-r <(tail -f filtering$pv) -w filtering$1 2>/dev/null & disown)
-	pid=$!
-	evidence $1 $pid
-}
-
 
 
 if [[ $# -eq 0 ]]
@@ -182,7 +199,8 @@ then
 	
 else
 	echo "OUT OF LINE file: $1"
-	cp $1 temp
+	cp $1 filtering0
+	let nr=1
 	PS3="Choose an option:"
 	select ITEM in "Statistics" "Alerts" "Exit"
 	do
@@ -205,31 +223,52 @@ else
 				break
 				;;
 				2) 
-				cp $1 temp
+				echo "Current level: $nr"
+				read -p "Introduce desired level: " new_nr
+				if [[ $new_nr -lt 0 ]]
+				then
+					echo $new_nr
+					echo "Invalid level"
+				else
+				
+				while [[ $nr -gt $new_nr ]]
+				do
+					rm filtering$nr 2> /dev/null
+					let nr=nr-1
+					echo $nr $new_nr
+				done
+				fi
 				break
 				;;
 				*) 
 				echo "Invalid option - eracing filters"
-				cp $1 temp
+				let nr=1
 				break
 			esac
 			done
 		
 		case $filter in
 			ts)
-			traffic_stat "temp"
+			traffic_stat $nr
 			;;
 			ips) 
-			ip_source "temp"
+			ip_source $nr
+			let nr=nr+1
 			;;
 			ipd)
-			ip_destination "temp"
+			ip_destination $nr
+			let nr=nr+1
 			;;
 			prot)
-			protocol "temp"
+			protocol $nr
+			let nr=nr+1
+			;;
+			intr)
+			interaction $nr
 			;;
 			pck)
-			sudo tshark -r temp -x
+			let lvl=nr-1
+			sudo tshark -r filtering$lvl -x
 			;;
 			*) 
 			echo "Invalid option"
